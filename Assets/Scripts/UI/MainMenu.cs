a43bc0ea887ee463154a8f540a702e2964ab469a
LEO-UWE <LAFrilot@outlook.com>
2025-07-06 19:22:26 -0700

    Main menu start

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private int trainingModeSceneIndex;
+    [SerializeField] private int demoModeSceneIndex;
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartTrainingMode()
+    {
+        SceneManager.LoadScene(trainingModeSceneIndex);
+    }
+
+    public void StartDemo()
+    {
+        SceneManager.LoadScene(demoModeSceneIndex);
+    }
+}
