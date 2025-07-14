4ee07b6eeea5867a85e87048fd095797cebf5dfe
Leo <LAFrilot@outlook.com>
2025-07-13 18:21:31 -0700

    Player and fist updates

--- Assets/Scripts/PlayerFistUI.cs
+++ Assets/Scripts/PlayerFistUI.cs
@@ -11,6 +11,9 @@ public class PlayerFistUI : MonoBehaviour
 
     public void FaceRight(bool faceRight)
     {
-        sprite.transform.localScale = new Vector3(1f, faceRight ? 1f : -1f, 1f);
+        sprite.transform.localScale = new Vector3(
+            (faceRight ? 1f : -1f) * Mathf.Abs(sprite.transform.localScale.x), 
+            sprite.transform.localScale.y, 
+            sprite.transform.localScale.z);
     }
 }
