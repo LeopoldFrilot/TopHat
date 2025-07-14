c457c5e0895b1578c14c35aa2c89ab66aa073e2b
Leo <LAFrilot@outlook.com>
2025-07-14 09:53:00 -0700

    Initializing FightScene2.0

---
+++ Assets/Scripts/_Rollback/THGameView.cs
@@ -0,0 +1,10 @@
+﻿using SharedGame;
+using UnityEngine;
+
+public class THGameView : MonoBehaviour, IGameView
+{
+    public void UpdateGameView(IGameRunner runner)
+    {
+        throw new System.NotImplementedException();
+    }
+}
