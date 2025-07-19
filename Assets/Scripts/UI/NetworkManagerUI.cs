a7351c439a58d8e99815e46765c67c9f91f488ee
Leo <LAFrilot@outlook.com>
2025-07-18 21:22:10 -0700

    Update NetworkManagerUI.cs

--- Assets/Scripts/UI/NetworkManagerUI.cs
+++ Assets/Scripts/UI/NetworkManagerUI.cs
@@ -5,11 +5,13 @@ public class NetworkManagerUI : MonoBehaviour
 {
     public void Host()
     {
+        GameWizard.Instance.SetGameAsNetworked();
         NetworkManager.Singleton.StartHost();
     }
 
     public void Join()
     {
+        GameWizard.Instance.SetGameAsNetworked();
         NetworkManager.Singleton.StartClient();
     }
 }
